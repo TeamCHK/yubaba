a873f1ce7f710c4012916dd8ea90c6c77b57a666
Jun Hur <junhur@andrew.cmu.edu>
2022-07-24 20:26:46 -0400

    Add TODO for website support

--- extension/src/ts/contentScript.ts
+++ extension/src/ts/contentScript.ts
@@ -7,7 +7,7 @@ const sendContent = (textToSend: string) => {
     };
     chrome.runtime.sendMessage(msg);
 };
-
+//TODO: Decide and change whether we'll serve all types of websites vs only certain websites
 if (window.location.href.includes('cnn.com') && window.location.href.includes('index.html')) {
     const body: HTMLElement = document.body;
     const bodyText: Array<string> = body.outerText.split("\n").filter(text => text);
