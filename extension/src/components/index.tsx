be53508771f0c490b761a3a3700fca35b1703287
Sungho Cho <sungh5c@gmail.com>
2022-07-30 19:08:36 +0900

    Move index.html to src/static/html/

--- extension/src/components/index.tsx
+++ extension/src/components/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot, Root } from "react-dom/client";
 import Popup from './popup';
-import "./index.css";
 
 const root: Root = createRoot(
     document.getElementById("react-target") as HTMLElement
