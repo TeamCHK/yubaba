811bd7adde3f5aada56db5952bc6e49a3f14aedc
Sungho Cho <sungh5c@gmail.com>
2022-08-11 17:35:20 +0900

    Update logging statements

--- aws/lambda/parse_article.py
+++ aws/lambda/parse_article.py
@@ -1,35 +1,33 @@
-import json
-import os
 import boto3
-from newspaper import Article
+import json
 import logging
+import os
+from newspaper import Article
 
 logging.root.setLevel(logging.DEBUG)
 
 ENDPOINT_NAME = os.environ['ENDPOINT_NAME']
 runtime = boto3.client('runtime.sagemaker')
 
-
 def lambda_handler(event, context):
-    logging.INFO("Received event: " + json.dumps(event, indent=2))
+    logging.info("Received event: " + json.dumps(event, indent=2))
     data = json.loads(json.dumps(event))
     url = data["url"]
-
-    logging.INFO(f"Downloading article from: {url}")
+    
+    logging.info(f"Downloading article from: {url}")
     article = Article(url)
     article.download()
-
-    logging.INFO(f"Parsing article from: {url}")
+    
+    logging.info(f"Parsing article from: {url}")
     article.parse()
-    print(article.title)
-    logging.DEBUG("Article title: ", article.title)
-    logging.DEBUG("Article date: ", article.publish_date)
-    logging.DEBUG("Article authors: ", article.authors)
-
+    logging.debug("Article title: ", article.title)
+    logging.debug("Article date: ", article.publish_date)
+    logging.debug("Article authors: ", article.authors)
+    
     inference_payload = {
         "inputs": [article.text]
     }
-
+    
     if not article.is_valid_body:
         return {
             "statusCode": 400,
@@ -37,13 +35,13 @@ def lambda_handler(event, context):
                 "url": url
             }]
         }
-
+    
     response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
                                        ContentType='application/json',
                                        Body=json.dumps(inference_payload))
     result = json.loads(response['Body'].read().decode())
-    print(result)
-
+    logging.debug(result)
+    
     return {
         'statusCode': 200,
         'body': result
