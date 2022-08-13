e96279e4b7c83bff6f37d479edf3f6df2e855cd6
Sungho Cho <sungh5c@gmail.com>
2022-08-14 00:21:04 +0900

    Add comments to lambda function

--- aws/lambda/lambda_function.py
+++ aws/lambda/lambda_function.py
@@ -10,24 +10,29 @@ ENDPOINT_NAME = os.environ['ENDPOINT_NAME']
 runtime = boto3.client('runtime.sagemaker')
 
 def handler(event, context):
+    # Extract URL from the request
     logging.info("Received event: " + json.dumps(event, indent=2))
     data = json.loads(json.dumps(event))
     url = data["url"]
     
+    # Download article from the given URL
     logging.info(f"Downloading article from: {url}")
     article = Article(url)
     article.download()
     
+    # Parse the article
     logging.info(f"Parsing article from: {url}")
     article.parse()
     logging.debug(f"Article title: {article.title}")
     logging.debug(f"Article date: {article.publish_date}")
     logging.debug(f"Article authors: {article.authors}")
     
+    # Set up inference payload that contains article body
     inference_payload = {
         "inputs": [article.text]
     }
     
+    # Send error response if the given URL does not contain a valid article
     if not article.is_valid_body:
         return {
             "statusCode": 400,
@@ -36,6 +41,7 @@ def handler(event, context):
             }]
         }
     
+    # Invoke an inference endpoint
     response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
                                        ContentType='application/json',
                                        Body=json.dumps(inference_payload))
