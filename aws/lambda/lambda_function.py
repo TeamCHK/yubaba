46d3bc1375cbe0a034b55d6acc536956f2ca4b17
Sungho Cho <sungh5c@gmail.com>
2022-08-14 00:10:01 +0900

    Add lambda_function along with Dockerfile, README, and requirements.txt

---
+++ aws/lambda/lambda_function.py
@@ -0,0 +1,48 @@
+import boto3
+import json
+import logging
+import os
+from newspaper import Article
+
+logging.root.setLevel(logging.DEBUG)
+
+ENDPOINT_NAME = os.environ['ENDPOINT_NAME']
+runtime = boto3.client('runtime.sagemaker')
+
+def handler(event, context):
+    logging.info("Received event: " + json.dumps(event, indent=2))
+    data = json.loads(json.dumps(event))
+    url = data["url"]
+    
+    logging.info(f"Downloading article from: {url}")
+    article = Article(url)
+    article.download()
+    
+    logging.info(f"Parsing article from: {url}")
+    article.parse()
+    logging.debug(f"Article title: {article.title}")
+    logging.debug(f"Article date: {article.publish_date}")
+    logging.debug(f"Article authors: {article.authors}")
+    
+    inference_payload = {
+        "inputs": [article.text]
+    }
+    
+    if not article.is_valid_body:
+        return {
+            "statusCode": 400,
+            "body": [{
+                "url": url
+            }]
+        }
+    
+    response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
+                                       ContentType='application/json',
+                                       Body=json.dumps(inference_payload))
+    result = json.loads(response['Body'].read().decode())
+    logging.debug(result)
+    
+    return {
+        'statusCode': 200,
+        'body': result
+    }
