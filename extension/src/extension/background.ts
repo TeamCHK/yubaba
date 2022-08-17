5bec7777550186c2a732924d31a96ac1c5da1d94
Sungho Cho <sungh5c@gmail.com>
2022-08-18 01:22:11 +0900

    Add temporary debugging code to background script

--- extension/src/extension/background.ts
+++ extension/src/extension/background.ts
@@ -2,17 +2,20 @@ import wretch from 'wretch';
 import { MLISRequest, MLISResponse } from './types';
 
 // TODO: Issue #14: Change url and endpoint after MLIS is ready
-const apiRoot: string = "http://localhost:8000";
+// const apiRoot: string = "http://localhost:8000";
+const apiRoot: string = "https://jlgl3bu3n6.execute-api.us-east-1.amazonaws.com/test/pass-article-to-summarization-model";
 const summarizationEndpoint: string = "/summary";
 
-
+console.log("background")
 try {
     chrome.runtime.onMessage.addListener(
         function (request: MLISRequest, _, sendResponse: (response: MLISResponse) => void) {
+            console.log("Message received")
             wretch(apiRoot + summarizationEndpoint)
                 .options({ mode: "cors" })
                 .post({ text: request.url })
                 .json((mlisResponse: MLISResponse) => {
+                    console.log(mlisResponse)
                     sendResponse(mlisResponse)
                 });
         }
